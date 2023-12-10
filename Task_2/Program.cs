ed988e2a8b0296b12c30539bb5e5d6d2329be521
Dmyrix <mangoost81@gmail.com>
2023-12-10 23:21:40 +0300

    add while

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,5 +1,20 @@
-﻿int[] array = {14,222,33,4,55,67,74,85};
+﻿int[] array = {14,222,33,4,55,67,74,55,85};
 
 //int n = array.Length;   //Количество элементов массива
 
-Console.WriteLine(array.Length);
+int find = 55;
+
+int index = 0;
+
+while (index < array.Length)
+{
+    if(array[index] == find)
+    {
+      Console.WriteLine(index);
+      break;
+    }
+    index = index + 1;
+
+}
+
+
